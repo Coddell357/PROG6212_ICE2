0c2dede8f0d981b54bc765c4ca5a3aa6c4b6e690
Coddell357 <ST10450661@vcconnect.edu.za>
2025-08-01 14:34:43 +0200

    Created the Index class and Unit Tests

---
+++ Indexers_ICE2/Heroes.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers_ICE2
+{
+    public class Heroes
+    {
+        private int age;
+        private string name, superPower, weakness, personality, background;
+
+        public Heroes(string name, int age, string superPower, string weakness, string personality, string background)
+        {
+            this.name = name;
+            this.age = age;
+            this.superPower = superPower;
+            this.weakness = weakness;
+            this.personality = personality;
+            this.background = background;
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                    return this.name;
+                else if (index == 1)
+                    return this.age;
+                else if (index == 2)
+                    return this.superPower;
+                else if (index == 3)
+                    return this.weakness;
+                else if (index == 4)
+                    return this.personality;
+                else if (index == 5)
+                    return this.background;
+                return null;
+            }
+            set
+            {
+                if (index == 0)
+                    this.name = (string)value; //sets value based on index
+                else if (index == 1)
+                    this.age = (int)value;
+                else if (index == 2)
+                    this.superPower = (string)value;
+                else if (index == 3)
+                    this.weakness = (string)value;
+                else if (index == 4)
+                    this.personality = (string)value;
+                else if (index == 5)
+                    this.background = (string)value;
+            }
+        }
+
+        public object this[string attrName]
+        {
+            get
+            {
+                if (attrName.ToLower().Equals("name"))
+                    return this.name;
+                else if (attrName.ToLower().Equals("age"))
+                    return this.age;
+                else if (attrName.ToLower().Equals("superpower"))
+                    return this.superPower;
+                else if (attrName.ToLower().Equals("weakness"))
+                    return this.weakness;
+                else if (attrName.ToLower().Equals("personality"))
+                    return this.personality;
+                else if (attrName.ToLower().Equals("background"))
+                    return this.background;
+                return null;
+                //looks at attribute name, converted to lower case and returns matching value
+            }
+            set
+            {
+                if (attrName.ToLower().Equals("name"))
+                    this.name = (string)value;
+                else if (attrName.ToLower().Equals("age"))
+                    this.age = (int)value;
+                else if (attrName.ToLower().Equals("superpower"))
+                    this.superPower = (string)value;
+                else if (attrName.ToLower().Equals("weakness"))
+                    this.weakness = (string)value;
+                else if (attrName.ToLower().Equals("personality"))
+                    this.personality = (string)value;
+                else if (attrName.ToLower().Equals("background"))
+                    this.background = (string)value;
+
+            }
+        }
+    }
+}
---
+++ TestHeroes/Test1.cs
@@ -0,0 +1,46 @@
+﻿using Indexers_ICE2;
+
+namespace TestHeroes
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestName()
+        {
+            Heroes hero = new Heroes("Wanda Maximoff", 30, "Chaos Magic", "Kids", "determined", "Older sister");
+            Assert.AreEqual(hero[0], "Wanda Maximoff");
+        }
+
+        [TestMethod]
+        public void TestAge()
+        {
+            Heroes hero = new Heroes("Black Widow", 33, "Skilled Assassin", "Sister", "Strong", "Spy");
+            Assert.AreEqual(hero[1], 33);
+        }
+
+        [TestMethod]
+        public void TestSuperPower()
+        {
+            Heroes hero = new Heroes("Loki", 27, "Sorcerer", "Love", "Funny", "Adopted Son");
+            hero[2] = "Illusion";
+            Assert.AreEqual(hero[2], "Illusion");
+        }
+
+        [TestMethod]
+        public void TestPersonality()
+        {
+            Heroes hero = new Heroes("Spiderman", 23, "Web abilities", "Relationships", "Multi-task", "Orphan");
+            Assert.AreEqual(hero["personality"], "Multi-task");
+        }
+
+        [TestMethod]
+        public void TestBackground()
+        {
+            Heroes hero = new Heroes("Iron Man", 40, "SuperHuman Suit", "Hackers", "Intelligent", "Domestic Worker");
+            hero[5] = "Father";
+            Assert.AreEqual(hero["background"], "Father");
+        }
+
+    }
+}
