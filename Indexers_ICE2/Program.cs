b7d1e5665cb5c969989d5eeefb521dce07cee94d
ShamgarVambe <128706559+ShamgarVambe@users.noreply.github.com>
2025-08-04 14:08:35 +0200

    coddell part 2
    done

--- Indexers_ICE2/Program.cs
+++ Indexers_ICE2/Program.cs
@@ -4,6 +4,41 @@
     {
         static void Main(string[] args)
         {
+            Heroes hero = new Heroes("Antman", 30, "Body-dismorphia", "aadvanced tech", "humour", "dad");
+            Heroes hero2= new Heroes("Ironman", 40, "Intelligence", "ego", "sarcasm", "billionaire");
+
+            hero[0] = "Antman";
+            Console.WriteLine("Name"+ hero[0]);
+            Console.WriteLine("Age:"+ hero[1]);
+            Console.WriteLine("Super Power:"+ hero[2]);
+            Console.WriteLine("Weakness:"+ hero[3]);
+            Console.WriteLine("Personality:"+ hero[4]);
+            Console.WriteLine("Background:"+ hero[5]);
+            
+         
+            Console.WriteLine("Name:"+ hero["name"]);
+            Console.WriteLine("Age:"+ hero["age"]);
+            Console.WriteLine("Super Power:"+ hero["superpower"]);
+            Console.WriteLine("Weakness:" +hero["weakness"]);
+            Console.WriteLine("Personality:" +hero["personality"]);
+            Console.WriteLine("Background:" +hero["background"]);
+
+            hero2[0] = "Ironman";
+            Console.WriteLine("Name" + hero2[0]);
+            Console.WriteLine("Age:" + hero2[1]);
+            Console.WriteLine("Super Power:" + hero2[2]);
+            Console.WriteLine("Weakness:" + hero2[3]);
+            Console.WriteLine("Personality:" + hero2[4]);
+            Console.WriteLine("Background:" + hero2[5]);
+
+
+            Console.WriteLine("Name:" + hero2["name"]);
+            Console.WriteLine("Age:" + hero2["age"]);
+            Console.WriteLine("Super Power:" + hero2["superpower"]);
+            Console.WriteLine("Weakness:" + hero2["weakness"]);
+            Console.WriteLine("Personality:" + hero2["personality"]);
+            Console.WriteLine("Background:" + hero2["background"]);
         }
+
     }
 }
